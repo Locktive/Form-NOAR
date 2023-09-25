5a532f50d557c70c347c6d13eaa9b30698eb8dce
Locktive <117493094+Locktive@users.noreply.github.com>
2023-09-25 09:46:18 -0300

    pagina login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs');
+  res.render('index.ejs', { title: 'Login - Bombeiros de Guaramirim' });
 });
 
 router.get('/form', function(req, res, next) {
